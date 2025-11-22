39554cfa9e21be15cfb3555de5433d5850d74f45
haroon760 <muhammadharoon2004059@gmail.com>
2025-11-22 10:59:24 +0500

    Chapter 9 to 11 added

---
+++ JS Assignments/chapter 9 & 11/app.js
@@ -0,0 +1,52 @@
+// let correctAnswer = "one"
+// let userAnswer = "one"
+
+
+// let score = 0;
+
+
+// if(correctAnswer == userAnswer) {
+//     ++score
+// }
+
+
+// console.log(score)
+
+
+// if (false)  console.log("My Name is Hasan")
+//     else console.log("false mil raha hai")
+    
+
+
+// let num = 12
+// if (num == 10 + 2) {
+//     console.log("Both values are equal")
+// }
+
+
+
+
+// let Temperature = prompt("Enter the Temperature");
+// if (isNaN(T)) {
+//     alert("Invalid input.Please enter a valid temperature");
+// }
+
+// if (T > 40) {
+//     alert("It is too hot outside.");
+// }
+
+// else if (T > 30) {
+//     alert("The weather today is normal.");
+// }
+
+// else if (T > 20) {
+//     alert ("Today's weather so cool.");
+// }
+
+// else if (T > 10) {
+//     alert ("OMG! Today's weather is so cool.");
+// }
+
+// else {
+//     alert("It is very cold outside.");
+// }
